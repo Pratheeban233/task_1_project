46a32c5fb801cf596304afc016ca929f270bb856
pratheeban <40929176+Pratheeban233@users.noreply.github.com>
2018-10-16 19:09:13 +0530

    Add files via upload

--- NewIndiaCorporation1.java
+++ NewIndiaCorporation1.java
@@ -2,9 +2,42 @@ package employee_salary;
 import java.util.*;
 public class NewIndiaCorporation1 {
 
-	/**
-	 * @param args
+	/*An organization needs a program to calculate how much to pay their hourly 
+	 * employees.  Employees should work for a minimum of 40 hours per week. 
+	 *  They will get Over Time(OT) salary if they work more than 40 hours.  
+	 *  OT salary is 1.5 times salary per extra hour.  
+	 *  The organization pays a minimum of Rs. 50 per hour. Though the organization 
+	 *  pays more for OT, it is very strict for employees personal time.  Hence,
+	 *   no employee is allowed to work more than 60 hours per week.  
+
+ Summary: 
+* An employee gets paid (hours worked) × (base pay), for each hour up to 40 hours. 
+
+* For every hour over 40, they get overtime = (base pay) × 1.5. 
+
+* The base pay must not be less than the minimum wage (Rs. 50 an hour). 
+
+* If it is less than Rs. 50, print an error message. 
+
+* If the number of hours is greater than 60, print an error message. 
+
+ 
+
+Direction: 
+Create a new class called NewIndiaCorporation. Write a method that takes the base 
+pay and hours worked as parameters, and prints the total pay or an error. Write a main 
+method that calls this method for each of these employees: 
+
+Base Pay  Hours Worked 
+
+Employee 1 Rs. 50 35 
+
+Employee 2 Rs. 45 47 
+
+Employee 3 Rs. 100 73 
 	 */
+	
+	
 	//int workHours;
 	//int salaryPay;
 	static double c;
